ed2184370b500e9c3e1aff8aaca770938cf073fd
CallbackAttack <yan.doschinskiy@gmail.com>
2021-03-27 02:27:58 +0700

    fix2

--- server/index.js
+++ server/index.js
@@ -22,8 +22,18 @@ if (!isDev && cluster.isMaster) {
   })
 } else {
   const app = express()
+  app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+      return res.status(200).json({})
+    }
+    next()
+  })
   app.use(bodyParser())
   app.use(cors())
+
 
   const mongoose = require("mongoose")
 
