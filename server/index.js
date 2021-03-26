100f163692725a1c601aa9b84e7d0cf355fa46d5
CallbackAttack <yan.doschinskiy@gmail.com>
2021-03-27 04:39:56 +0700

    fix4

--- server/index.js
+++ server/index.js
@@ -22,17 +22,7 @@ if (!isDev && cluster.isMaster) {
 } else {
   const app = express()
 
-  const setHeaders = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    )
-    next()
-  }
-
-  server.use(setHeaders())
+  app.use(bodyParser())
 
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
