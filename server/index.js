1f6feeb2f67592aa867dc979d79f336b1ffd5b1d
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2021-06-15 13:11:35 +0700

    authorization api added, hash password and jwt token generation functions added

--- server/index.js
+++ server/index.js
@@ -1,43 +1,57 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const path = require("path")
-const cluster = require("cluster")
-const numCPUs = require("os").cpus().length
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { secret } = require("./config");
+// const { check } = require("express-validator");
+// const { body, validationResult } = require("express-validator/check");
+// const { sanitizeBody } = require("express-validator/filter");
 
-const isDev = process.env.NODE_ENV !== "production"
-const PORT = process.env.PORT || 5000
+const generateAccessToken = (id, email) => {
+  const payload = {
+    id,
+    email,
+  };
+  return jwt.sign(payload, secret);
+};
+
+const isDev = process.env.NODE_ENV !== "production";
+const PORT = process.env.PORT || 5000;
 
 if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`)
+  console.error(`Node cluster master ${process.pid} is running`);
 
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
+    cluster.fork();
   }
 
   cluster.on("exit", (worker, code, signal) => {
     console.error(
       `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
-    )
-  })
+    );
+  });
 } else {
-  const app = express()
+  const app = express();
 
-  app.use(bodyParser())
+  app.use(bodyParser());
 
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    )
-    next()
-  })
+    );
+    next();
+  });
 
-  const mongoose = require("mongoose")
+  const mongoose = require("mongoose");
 
   const dbUrl =
-    "mongodb+srv://testAdmin:1246@cluster1.klgod.mongodb.net/testdb?retryWrites=true&w=majority"
+    "mongodb+srv://testAdmin:1246@cluster1.klgod.mongodb.net/testdb?retryWrites=true&w=majority";
 
   mongoose
     .connect(dbUrl, {
@@ -45,7 +59,7 @@ if (!isDev && cluster.isMaster) {
       useUnifiedTopology: true,
     })
     .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log("Connection error - ", err))
+    .catch((err) => console.log("Connection error - ", err));
 
   const PhonesSchema = new mongoose.Schema({
     name: {
@@ -56,49 +70,112 @@ if (!isDev && cluster.isMaster) {
       type: String,
       required: true,
     },
-  })
+  });
 
-  const Phone = mongoose.model("phones", PhonesSchema)
+  const UserSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    token: { type: String, required: false },
+  });
 
-  app.get("/api/v1/getphones", (req, res) => {
-    const phoneDB = Phone.find({}).exec()
-    setTimeout(() => {
-      phoneDB.then((it) => res.send({ status: "successful", data: it }))
-    }, 0)
-  })
+  const Phone = mongoose.model("phones", PhonesSchema);
+  const User = mongoose.model("users", UserSchema);
 
-  app.post("/api/v1/getphones/filterByName", (req, res) => {
-    const phoneDB = Phone.find({}).exec()
-    setTimeout(() => {
-      phoneDB.then((it) => {
-        let positive
-        it.map((item) => {
-          if (item.name.toLowerCase() === req.body.name.toLowerCase()) {
-            positive = { status: "successful", data: [item] }
+  app.get("/api/v1/checkEmail", (req, res) => {
+    const usersDB = User.find({}).exec();
+    usersDB
+      .then((it) => {
+        it.find((item) => {
+          if (item.email.toLowerCase() === req.query.email.toLowerCase()) {
+            res.send({
+              status: "successful",
+              data: req.query.email,
+              message: `${req.query.email} is already registered`,
+              registered: true,
+            });
           }
-        })
-        const negative = { status: "not found" }
-        res.send(positive || negative)
+        });
+        res.send({
+          status: "successful",
+          data: null,
+          message: `${req.query.email} not found in the database`,
+          registered: false,
+        });
       })
-    }, 0)
-  })
+      .catch((err) => res.send({ status: "error", error: err }));
+  });
 
-  app.post("/api/v1/getphones/create/", (req, res) => {
-    Phone.create({ ...req.body })
-      .then(() => res.send({ status: "succesfull", name: req.body.name }))
-      .catch((err) => res.send({ status: "error" }))
-  })
+  app.post("/api/v1/register/", (req, res) => {
+    if (!req.body.email) {
+      res.status(422).json({ status: 400, message: "email was not sent" });
+    }
+    if (!req.body.password) {
+      res.status(422).json({ status: 400, message: "passward was not sent" });
+    }
+    const hashPassword = bcrypt.hashSync(req.body.password, 10);
+    User.create({ email: req.body.email.toLowerCase(), password: hashPassword })
+      .then(() =>
+        res.send({
+          status: "successful",
+          email: req.body.email.toLowerCase(),
+          message: "registration completed",
+        })
+      )
+      .catch((err) => res.send({ status: "error", error: err }));
+  });
 
-  app.delete("/api/v1/getphones/delete", (req, res) => {
-    Phone.deleteMany({ name: req.body.name })
-      .then(() => res.send({ status: "deleted", name: req.body.name }))
-      .catch((err) => res.send({ status: "error" }))
-  })
-  app.listen(PORT, function () {
-    console.error(
-      `Node ${
-        isDev ? "dev server" : "cluster worker " + process.pid
-      }: listening on port ${PORT}`
-    )
-  })
+  app.post("/api/v1/login", (req, res) => {
+    if (!req.body.email) {
+      res.send({ status: "error", message: "email was not sent" });
+    }
+    if (!req.body.password) {
+      res.send({ status: "error", message: "password was not sent" });
+    }
+
+    User.findOne({ email: req.body.email.toLowerCase() })
+      .exec()
+      .then((user) => {
+        if (user) {
+          const isValidPassword = bcrypt.compareSync(
+            req.body.password,
+            user.password
+          );
+          if (isValidPassword) {
+            const token = generateAccessToken(user._id, user.email);
+            User.updateOne(
+              { email: req.body.email.toLowerCase() },
+              { token: token },
+              function (err) {
+                if (err) {
+                  console.log(err);
+                }
+              }
+            );
+            res.send({
+              status: "successful",
+              message: `you are logged in`,
+              token: token,
+            });
+          } else {
+            res.send({
+              status: "error",
+              message: `invalid password`,
+            });
+          }
+        } else {
+          res.send({
+            status: "error",
+            message: `${req.body.email} not found in the database`,
+          });
+        }
+      })
+      .catch((err) => res.send({ status: "error", error: err }));
+  });
 }
