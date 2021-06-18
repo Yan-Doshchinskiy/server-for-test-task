e4d775382b35f5be5afd2da56515d68067d39554
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2021-06-18 22:48:35 +0700

    port changed

--- server/websocket.js
+++ server/websocket.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 
-const server = new WebSocket.Server({ port: 1000 });
+const server = new WebSocket.Server();
 
 server.on("connection", (ws) => {
   ws.on("message", (message) => {
