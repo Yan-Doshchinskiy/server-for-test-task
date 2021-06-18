536ec43ce2b019845298cc78268ab456a09dbe84
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2021-06-18 23:37:42 +0700

    optionsg

--- server/websocket.js
+++ server/websocket.js
@@ -1,44 +1,16 @@
-// const WebSocket = require("ws");
-
-// const server = new WebSocket.Server({ port: 1000 });
-
-// server.on("connection", (ws) => {
-//   ws.on("message", (message) => {
-//     server.clients.forEach((client) => {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send(message);
-//       }
-//     });
-//   });
-//   ws.send("Добро пожаловать в Chat");
-// });
-
-const express = require("express");
 const WebSocket = require("ws");
-const http = require("http");
 
-const app = express();
-const port = 8080;
+const server = new WebSocket.Server({ port: 1000 });
 
-const server = http.createServer(app);
-
-app.get("/", function (req, res, next) {
-  return res.send("Hello World!");
-});
-
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
+server.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    server.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
-  ws.send("something from server");
+  ws.send("Добро пожаловать в Chat");
 });
 
-server.listen(port, function (err) {
-  if (err) {
-    throw err;
-  }
-  console.log(`wss`, wss?.port);
-  console.log(`listening on port ${port}!`);
-});
+console.log("server.options.port", server.options.port);
